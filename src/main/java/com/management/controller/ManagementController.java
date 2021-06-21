541e83111c299257d2d29449c931f69f906ef142
Pastelhorizon <42344969+Pastelhorizon@users.noreply.github.com>
2021-06-21 19:07:41 +0530

    Updated Request Mappings to GetMapping and PostMapping

--- src/main/java/com/management/controller/ManagementController.java
+++ src/main/java/com/management/controller/ManagementController.java
@@ -5,11 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.management.client.AuthClient;
@@ -30,52 +30,52 @@ public class ManagementController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ManagementController.class);
 
-	@RequestMapping(path = "/", method = RequestMethod.GET)
+	@GetMapping(path = "/")
 	public ResponseEntity<?> healthCheckup() {
 		LOGGER.info("AWS Health Check");
 		return new ResponseEntity<>("", HttpStatus.OK);
 	}
 
-	@RequestMapping(path = "/collateral/getCollaterals/{loanid}", method = RequestMethod.GET)
+	@GetMapping(path = "/collateral/getCollaterals/{loanid}")
 	public ResponseEntity<?> getCollateralByLoanId(@RequestHeader(name = "Authorization") String token,
 			@PathVariable int loanid) {
 		LOGGER.info("Starting Collateral By Loan Id");
 		AuthResponse authResponse = authclient.verifyToken(token);
-//		LOGGER.info(authResponse.toString());
+		// LOGGER.info(authResponse.toString());
 		if (authResponse.isValid()) {
 			try {
 				DataCollateralLoan collateralLoan = managementService.getCollateralLoan(loanid);
-//				LOGGER.info(collateralLoan.toString());
-//				LOGGER.info("Ending Collateral By Loan Id");
+				// LOGGER.info(collateralLoan.toString());
+				// LOGGER.info("Ending Collateral By Loan Id");
 				return new ResponseEntity<DataCollateralLoan>(collateralLoan, HttpStatus.OK);
 
 			} catch (NoCollateralLoanFoundException e) {
 				LOGGER.error(e.getMessage());
-//				LOGGER.info("Ending Collateral By Loan Id");
+				// LOGGER.info("Ending Collateral By Loan Id");
 				return new ResponseEntity<>("Loan not Found", HttpStatus.NOT_FOUND);
 			}
 
 		} else {
 			LOGGER.error("Token Expired Please Create new to Use");
-//			LOGGER.info("Ending Get Menu Item By ID");
+			// LOGGER.info("Ending Get Menu Item By ID");
 			return new ResponseEntity<>("Token Expired Please Create new to Use", HttpStatus.FORBIDDEN);
 		}
 	}
 
-	@RequestMapping(path = "/collateral/saveCollateral", method = RequestMethod.POST)
+	@PostMapping(path = "/collateral/saveCollateral")
 	public ResponseEntity<?> saveCollateral(@RequestHeader(name = "Authorization") String token,
 			@RequestBody CollateralLoan collateralLoan) {
 		LOGGER.info("Starting save Collateral");
 		AuthResponse authResponse = authclient.verifyToken(token);
-//		LOGGER.info(authResponse.toString());
+		// LOGGER.info(authResponse.toString());
 		if (authResponse.isValid()) {
 			String saveCollateralLoan = managementService.saveCollateralLoan(collateralLoan);
-//			LOGGER.info(saveCollateralLoan.toString());
-//			LOGGER.info("Ending save Collateral");
+			// LOGGER.info(saveCollateralLoan.toString());
+			// LOGGER.info("Ending save Collateral");
 			return new ResponseEntity<>(saveCollateralLoan, HttpStatus.OK);
 		} else {
 			LOGGER.error("Token Expired Please Create new to Use");
-//			LOGGER.info("Ending Get Menu Item By ID");
+			// LOGGER.info("Ending Get Menu Item By ID");
 			return new ResponseEntity<>("Token Expired Please Create new to Use", HttpStatus.FORBIDDEN);
 		}
 	}
